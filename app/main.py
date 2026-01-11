7dabc80642381bb1ce98bd81642e7aed262b4f17
respo-git-service-account <service.account@respo.co.in>
2026-01-11 17:02:24 +0530

    Update app

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     version = os.getenv("VERSION", "v1.1.0")
-    return {"Hello": "World", "Version": version, "Message": "GitOps with ArgoCD is working!"}
+    return {"Hello": "World!!", "Version": version, "Message": "GitOps with ArgoCD is working!"}
 
 @app.get("/health")
 def health_check():
